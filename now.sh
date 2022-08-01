750a66cd87be05b4c3b63701671895e3bce4cdce
MrbTd <mrbtd2000@gmail.com>
2022-08-01 13:44:04 +0000

    handle pusher

---
+++ now.sh
@@ -0,0 +1,4 @@
+now secret add pusher_app_id 1455655
+now secret add pusher_app_key 804482278d6ab26d6300
+now secret add pusher_app_secret 7950bffd0228efe35717
+now secret add pusher_app_cluster eu
---
+++ server.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+var express = require('express');
+var bodyParser = require('body-parser');
+var Pusher = require('pusher');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var pusher = new Pusher({//connect to pusher
+    appId: process.env.APP_ID,
+    key: process.env.APP_KEY,
+    secret: process.env.APP_SECRET,
+    cluster: process.env.APP_CLUSTER,
+});
+
+app.get('/', function (req, res) {//for testing if the server is running
+    res.send('all is well...');
+});
+
+//for authenticating users
+app.get("/pusher/auth", function (req, res) {
+    var query = req.query;
+    var socketId = query.socket_id;
+    var channel = query.channel_name;
+    var callback = query.callback;
+
+    var auth = JSON.stringify(pusher.authorizeChannel(socketId, channel));
+    var cb = callback.replace(/\"/g, "") + "(" + auth + ");";
+
+    res.set({
+        "Content-Type": "application/javascript"
+    });
+
+    res.send(cb);
+});
+
+app.post('/pusher/auth', function (req, res) {
+    var users = ['luz', 'vi', 'minda'];
+    var username = req.body.username;
+
+    if (users.indexOf(username) !== -1) {
+        var socketId = req.body.socket_id;
+        var channel = req.body.channel_name;
+        var auth = pusher.authorizeChannel(socketId, channel);
+        res.send(auth);
+    }
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port);
